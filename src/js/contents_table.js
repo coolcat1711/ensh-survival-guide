17dbe74bde14e6b0d8b74b32c974058a4896048b
coolcat1711 <mackinderantoney@gmail.com>
2024-07-15 19:58:35 -0400

    Removes h1 (Main Title) from contents table

--- src/js/contents_table.js
+++ src/js/contents_table.js
@@ -82,7 +82,7 @@ class ContentsList extends HTMLDivElement
 
     getHeaderList()
     {
-        return document.querySelectorAll("h1,h2,h3,h4,h5,h6")
+        return document.querySelectorAll("h2,h3,h4,h5,h6")
     }
 }
 
